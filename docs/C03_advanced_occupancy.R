5b48efea2e1bc592f7e31df579165dbc7d64b760
Diana Bowler <diana.e.bowler@gmail.com>
2021-08-28 20:42:00 +0200

    Add files via upload
    update practical

--- docs/C03_advanced_occupancy.R
+++ docs/C03_advanced_occupancy.R
@@ -24,6 +24,7 @@
 #   Modeling distribution, abundance and species richness using R and BUGS
 #   Volume 1: Prelude and Static models
 #   by Marc Kery & J. Andy Royle
+#   https://www.mbr-pwrc.usgs.gov/pubanalysis/keryroylebook/
 
 # Read in libraries that we will need
 
@@ -37,17 +38,19 @@ library(tidyverse)
 
 ### Dynamic models: inferring colonization and extinction ####
 
-set.seed(13973)
+set.seed(20)
 
 # Lets start with a simulation
 M <- 50                                 # Number of sites 
 J <- 3                                  # num secondary sample periods (i.e. visits)
 T <- 10                                 # num primary sample periods (i.e., years)
 
-#parameters
+#empty frames for data
 psi <- rep(NA, T)                       # Occupancy probability
 muZ <- z <- array(dim = c(M, T))        # Expected and realized occurrence
 y <- array(NA, dim = c(M, J, T))        # Detection histories
+
+#parameters
 psi[1] <- 0.4                           # Initial occupancy probability
 p <- runif(n=T, min=0.2, max=0.3)     # Detection probability
 phi <- runif(n=T-1, min=0.6, max=0.8)   # Persistence probability (1-epsilon, extinction)   
@@ -62,7 +65,9 @@ z[,1] <- rbinom(M, 1, psi[1])           # Initial occupancy state
 for(i in 1:M){                          # Loop over sites
   for(k in 2:T){                        # Loop over years
     muZ[k] <- z[i, k-1]*phi[k-1] + (1-z[i, k-1])*gamma[k-1]
-    z[i,k] <- rbinom(1, 1, muZ[k])}}
+    z[i,k] <- rbinom(1, 1, muZ[k])
+  }
+}
 
 
 #Observation process:
@@ -79,32 +84,30 @@ for(i in 1:M){
 dim(y)
 
 #plot simulated data
-propOcc <- colMeans(z)
+trueOcc <- colMeans(z)
 obsOcc <- colMeans(apply(y, c(1,3), max))
 
-ggplot(data.frame(Year=1:T, propOcc, obsOcc)) +
-  geom_line(aes(x = Year, y = propOcc)) +
+ggplot(data.frame(Year=1:T, trueOcc, obsOcc)) +
+  geom_line(aes(x = Year, y = trueOcc)) +
   geom_line(aes(x = Year, y = obsOcc), color = "red", linetype = "dashed")+
   theme_classic()
 
 #Lets fit the model with unmarked
-library(unmarked)
-
 
 #first, we need to format the data for unmarked
 
 #organise the reponse long format
-yy <- matrix(y, M, J*T)
+y_long <- matrix(y, M, J*T)
 
 #compare
 y[1,,]
-yy[1,]
+y_long[1,]
 
 #make spatio-temporal time variable
 year <- matrix(c('01','02','03','04','05','06','07','08','09','10'),
-               nrow(yy), T, byrow=TRUE)
+               nrow(y_long), T, byrow=TRUE)
 
-simUMF <- unmarkedMultFrame(y = yy,
+simUMF <- unmarkedMultFrame(y = y_long,
                             yearlySiteCovs = list(year = year),
                             numPrimary=T)
 summary(simUMF)
@@ -117,9 +120,10 @@ m0 <- colext(psiformula= ~ 1,       # First-year occupancy
              data = simUMF)
 summary(m0)
 
+#extract components separately
 names(m0)#we can extract all these elements
-backTransform(m0, type="psi")
-confint(backTransform(m0, type="psi"))
+backTransform(m0, type="col")
+confint(backTransform(m0, type="col"))
 
 #model with year effects
 m1 <- colext(psiformula = ~1,   
@@ -135,14 +139,14 @@ predict(m1, type='ext')
 predict(m1, type='col')
 predict(m1, type='det')
 
-#Lets look at predicted occupancy each year
+#Lets look at predicted occupancy each year and get 95%CI
 predictOccupancy <- function(fm) {
   
   psi.hat <- plogis(coef(fm, type="psi"))
-  T <- fm@data@numPrimary
   gamma.hat <- plogis(coef(fm, type="col"))
   phi.hat <- 1 - plogis(coef(fm, type="ext"))
   
+  T <- fm@data@numPrimary
   for(t in 2:T) {
     psi.hat[t] <- psi.hat[t-1]*phi.hat[t-1] +(1-psi.hat[t-1])*gamma.hat[t-1]
   }
@@ -151,16 +155,16 @@ predictOccupancy <- function(fm) {
 
 #also see: projected(m1)
 
-pb <- parboot(m1, statistic = predictOccupancy, nsim=50)#increase to 1000
+pb <- parboot(m1, statistic = predictOccupancy, nsim=20)#increase to 100
 predsCI <- data.frame(cbind(pb@t0,t(apply(pb@t.star, 2, quantile, probs=c(0.025, 0.975)))))
 colnames(predsCI) <- c("estimate", "lower", "upper")
 
 #plot data
-ggplot(data.frame(Year=1:T, propOcc, obsOcc, 
+ggplot(data.frame(Year=1:T, trueOcc, obsOcc, 
                   preds = predsCI$estimate,
                   lower = predsCI$lower, 
                   upper = predsCI$upper)) +
-  geom_line(aes(x = Year, y = propOcc)) +
+  geom_line(aes(x = Year, y = trueOcc)) +
   geom_line(aes(x = Year, y = obsOcc), color = "red", linetype = "dashed")+
   geom_ribbon(aes(x = Year, y = preds, ymin = lower, ymax = upper), fill = "blue", alpha=0.4)+
   theme_classic()
@@ -183,21 +187,27 @@ head(frogData)
 str(frogData)
 
 sort(unique(frogData$SurveyYear))
-sort(unique(frogData$RouteNumStopNum))
+length(unique(frogData$RouteNumStopNum))
 
-#make response binary
-frogData$Pcru <- ifelse(frogData$Pcru>0, 1, 0)
-
-#make short to keep the computation times quick
+#make data frame shorter to keep the computation times quick
 frogData <- subset(frogData, RouteNumStopNum %in% 
                      unique(frogData$RouteNumStopNum)[1:20])
 frogData$RouteNumStopNum <- factor(frogData$RouteNumStopNum)
+
+#scale data
+frogData$JulianDate <- as.numeric(scale(frogData$JulianDate))
+frogData$MinAfterSunset <- as.numeric(scale(frogData$MinAfterSunset))  
+
+#do we have repeat visits per year?
 table(frogData$RouteNumStopNum,frogData$SurveyYear)
 #between 2 and 4 visits per year
 
-#fit using unmarked
+#make response binary (just for teaching purposes!)
+frogData$Pcru <- ifelse(frogData$Pcru>0, 1, 0)
+
+
+#fit using unmarked for comparison
 umf <- formatMult(frogData)
-obsCovs(umf) <- scale(obsCovs(umf))
 
 ## constant transition rates
 (fm <- colext(psiformula = ~ 1,
@@ -206,10 +216,19 @@ obsCovs(umf) <- scale(obsCovs(umf))
               pformula = ~ JulianDate + MinAfterSunset, umf, control = list(trace=1, maxit=1e4)))
 
 #we can plot the effect of each covariate, like, usual, with a predict function
-predict(fm, type="p", newdata, appendData=TRUE)
+predDF <- predict(fm, type="det", newdata = data.frame(JulianDate = mean(frogData$JulianDate), 
+                                           MinAfterSunset = seq(min(frogData$MinAfterSunset),
+                                                                max(frogData$MinAfterSunset),
+                                                                length.out=100)), appendData=TRUE)
 
-##estimates of population parameters
-projected(fm)
+ggplot(predDF)+
+  geom_line(aes(x = MinAfterSunset, y = Predicted))+
+  geom_ribbon(aes(x = MinAfterSunset, 
+                  ymin = Predicted - 1.96 * SE,
+                  ymax = Predicted + 1.96 * SE),alpha=0.2)+
+  theme_classic() +
+  ylab("Predicted Detection Probability")
+
 
 # same example in JAGS
 
@@ -290,8 +309,8 @@ jags.data <- list(nsite = n_distinct(frogData$RouteNumStopNum),
                   y = frogData$Pcru,
                   siteIndex = as.numeric(as.factor(frogData$RouteNumStopNum)),
                   yearIndex = as.numeric(as.factor(frogData$SurveyYear)),
-                  date = as.numeric(scale(frogData$JulianDate)),
-                  time = as.numeric(scale(frogData$MinAfterSunset)))
+                  date = frogData$JulianDate,
+                  time = frogData$MinAfterSunset)
 
 
 # Initial values
@@ -308,10 +327,13 @@ ni <- 2000   ;   nt <- 2   ;   nb <- 500   ;   nc <- 3
 # Call JAGS 
 library(rjags)
 library(jagsUI)
-fm_dynamic <- jags(jags.data, inits, params, "dynOccu.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
+fm_dynamic <- jags(jags.data, inits, params, "dynOccu.txt", n.chains = nc, n.thin = nt, n.iter = ni,
+                   n.burnin = nb, parallel = TRUE)
 
+#look at model coefficients
 fm_dynamic
 
+#check model convergence
 plot(fm_dynamic)
 
 ### False presence ##############################################
@@ -324,124 +346,63 @@ plot(fm_dynamic)
 #type 2 - false negative and false positive detection probabilities
 #type 3 - certain detections and uncertain detections that may include false positive detections. Uncertain detections are given a value of 1 and certain detections a value of 2.
 
-n = 100 # number of sites
-o = 10 #total number of occasions 
-o1 = 5 #number of occasions with data type 1
+#sampling
+nsites = 100 # number of sites
+nsurveys_type1 = 5 #total number of occasions (i.e. repeat visits) with type 1 data
+nsurveys_type2 = 5 #total number of occasions (i.e. repeat visits) with type 2 data
 
-#simulate each data type
-y = matrix(NA,n,o)
+#parameters
+psi <- 0.5 #occupancy probability
+p <- c(0.5,0.3)   #detection probability of method 1 and method 2
+fp <- 0.05 #false positive probability
 
-#data type 1
-r = 0.5 
-y[1:(n*0.5),1:(o-o1)] <- rbinom((o-o1)*n*0.5,1,r) #first 5 occassions - data type 1
+#simulate data
+z <- rbinom(nsites, 1, psi)
+y <- matrix(NA, nrow = nsites, ncol = nsurveys_type1 + nsurveys_type2)
+for(i in 1:nsites){
+  p1 <- p[1] * z[i]
+  p2 <- p[2] * z[i]
+  y[i,1:5] <- rbinom(nsurveys_type1, 1, p1)
+  y[i, 6:10] <- rbinom(nsurveys_type2, 1, p2)
+}
 
-#data type 2
-p = 0.7
-y[1:(n*0.5),(o-o1+1):o] <- rbinom((n*o1*0.5),1,p) # second 5 occasions - data type 2
+#method covariate 
+Method = matrix(c(rep("1",nsurveys_type1),rep("2",nsurveys_type2)), nrow = nsites,
+                ncol = nsurveys_type1 + nsurveys_type2, byrow = TRUE)
+type = c(nsurveys_type1, nsurveys_type2, 0) 
 
-#add false positives to data type 2
-fp = 0.05
-y[(n*0.5+1):n,(o-o1+1):o] <- rbinom((n*o1*0.5),1,fp)
-
-#create a covariate telling R of the amount of each data type
-type <- c((o-o1),o1,0)  
-
-#create observation-level data frame
-occ <- matrix(c(rep(0,n*(o-o1)),rep(1,n*o1)),n,o)
-occocc <- list(METHOD = occ)
-
-#create site-level data
-site <- data.frame(habitat = c(rep(1,n*0.5*.8),rep(0,n*0.5*.2),rep(1,n*0.5*.2),rep(0,n*0.8*0.5)))
-
-#package up for unmarked
-umf1 <- unmarkedFrameOccuFP(y, 
-                            siteCovs = site, 
-                            obsCovs = occ, 
+#package data for unmarked
+umf1 <- unmarkedFrameOccuFP(y = y, 
+                            obsCovs = list(Method = Method),
                             type = type)
 
-m1 <- occuFP(detformula = ~ METHOD, 
-             FPformula = ~1,#covariates of false positive detection probability
-             Bformula = ~1, #covariates of probability detections are certain
+#fit model
+m1 <- occuFP(detformula = ~ Method, 
+             FPformula = ~ 1,#covariates of false positive detection probability
              stateformula = ~ 1, 
              data = umf1)
 
 #summary
 m1
 
-#we can also:
-coef(m1)
-confint(m1, type = 'det')
-
-#components of model
+#predicted probability
 names(m1)
-predict(m1, type = 'fp') 
-predict(m1, type = 'state') 
-predict(m1, type = 'det')
+predict(m1, type="det", newdata = data.frame(Method=c("1","2")))
+predict(m1, type="state", newdata = data.frame(Method=c("1","2")))
 
+### Example code for community models ##############
 
-### Metacommunity data from the Swiss breeding bird survey MHB ####
+#see Kery and Royle for full example
 
-#taken from Kery and Royle AHM book
-
+#could be analysed with e.g.:
 data(MHB2014)
 ?MHB2014
-str(MHB2014)
-
-# Check the detection data in 3D array MHB2014$count: site x rep x species
-nsite <- nrow(MHB2014$sites)    # number of sites in Swiss MHB
-nrep <- 3                           # maximum number of replicate surveys per season
-nspec <- nrow(MHB2014$species)   # 158 species occur in the 2014 data
-dim(MHB2014$count) == c(nsite, nrep, nspec) # check
-
-# Create the detection/nondetection (1/0) array
-y <- MHB2014$count ; y[y > 1] <- 1  ## 'Y' replaced with 'y'
-str(y)
-
-# Check data for one species, here chaffinch, and pull them out from 3D array
-(tmp <- y[, , "Common Chaffinch"])
-
-# Frequency distribution of number of surveys actually carried out per site in 2014
-# NB MHB2014$sites$nsurvey gives the number of surveys *planned*.
-table(nsurveys <- apply(!is.na(y[,,1]), 1, sum))
-
-# Which site has all NA data in 2014 ?
-(NAsites <- which(nsurveys == 0) )
-
-# Observed number of occupied sites
-tmp <- apply(y, c(1,3), max, na.rm = TRUE)
-# For the 'all NA' site, max returns -Inf with a warning
-tmp[tmp == -Inf] <- NA         # Change -Inf to NA
-sort(obs.occ <- apply(tmp, 2, sum, na.rm = TRUE))
-
-# Plot species 'occurrence frequency' distribution (not shown)
-plot(sort(obs.occ), xlab = "Species number", ylab = "Number of quads with detections")
-
-# Drop data from species that were not observed in 2014
-toss.out <- which(obs.occ == 0)
-y <- y[,,-toss.out]
-obs.occ <- obs.occ[-toss.out]
-
-# Redefine nspec as the number of species observed in 2014: 145
-(nspec <- dim(y)[3])
-
-str(y)
-
-# Get observed number of species per site
-tmp <- apply(y, c(1,3), max, na.rm = TRUE)
-tmp[tmp == "-Inf"] <- NA
-sort(C <- apply(tmp, 1, sum))     # Compute and print sorted species counts
-
-plot(table(C), xlim = c(0, 60), xlab = "Observed number of species", ylab = "Number of quadrats", frame = FALSE)
-abline(v = mean(C, na.rm = TRUE), col = "blue", lwd = 3)
-
+head(MHB2014$counts)
 
 ### Independent species-specific random effects ####
 
-# Bundle and summarize data set
-str( win.data <- list(ysum = ysum, M = nrow(ysum), J = MHB2014$sites$nsurvey, nspec = dim(ysum)[2], R = matrix(c(5,0,0,1), ncol = 2), df = 3) )
-
 # Specify model in BUGS language
-sink("model6.txt")
+sink("model.txt")
 cat("
          model {
         
@@ -484,23 +445,6 @@ cat("
 sink()
 
 
-# Initial values
-zst <- apply(y, c(1,3), max) # Observed occurrence as starting values for z
-zst[is.na(zst)] <- 1
-inits <- function() list(z = zst, Omega = matrix(c(1,0,0,1), ncol = 2), eta = matrix(0, nrow = nspec, ncol = 2))
-
-# Parameters monitored
-params <- c("mu.eta", "probs", "psi", "p", "Nsite", "Nocc.fs", "Sigma", "rho")
-
-# MCMC settings
-ni <- 20000   ;   nt <- 15   ;   nb <- 5000   ;   nc <- 3
-
-# Call JAGS from R (ART 12 min), check traceplots and summarize posteriors
-out6 <- jags(win.data, inits, params, "model6.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
-par(mfrow = c(3,3))   ;   traceplot(out6, c('mu.eta', 'probs', 'Sigma', 'rho'))
-print(out6, 3)
-
-
 ### The Dorazio-Royle (DR) community occupancy model with data augmentation ####
 
 # Augment data set (DA part)
@@ -512,7 +456,7 @@ yaug <- cbind(ysum, array(0, dim=c(nsite, nz))) # Add all zero histories
 str( win.data <- list(yaug = yaug, nsite = nrow(ysum), nrep = MHB2014$sites$nsurvey, M = M, nspec = nspec, nz = nz) )
 
 # Specify model in BUGS language
-sink("model9.txt")
+sink("model.txt")
 cat("
         model {
         
@@ -567,48 +511,7 @@ cat("
         ",fill = TRUE)
 sink()
 
-# Initial values
-wst <- rep(1, nspec+nz)                   # Simply set everybody at 'occurring'
-zst <- array(1, dim = c(nsite, nspec+nz)) # ditto for z
-inits <- function() list(z = zst, w = wst, lpsi = rnorm(n = nspec+nz), lp = rnorm(n = nspec+nz))
 
-# Parameters monitored
-params <- c("mu.lpsi", "sd.lpsi", "mu.lp", "sd.lp", "psi", "p", "Nsite", "Ntotal", "omega", "n0")
-
-# MCMC settings
-ni <- 22000   ;   nt <- 2   ;   nb <- 2000   ;   nc <- 3
-
-# Call JAGS from R (ART 62 min), check convergence and summarize posteriors
-out9 <- jags(win.data, inits, params, "model9.txt", n.chains = nc, n.thin = nt, n.iter = ni, n.burnin = nb, parallel = TRUE)
-par(mfrow = c(2,2)) ; traceplot(out9, c('mu.lpsi', 'sd.lpsi', 'mu.lp', 'sd.lp'))
-print(out9, dig = 3)
-
-
-# Plot posterior distribution of site-specific species richness (Nsite)
-par(mfrow = c(3,3), mar = c(5,4,3,2))
-for(i in 1:267){
-  plot(table(out9$sims.list$Nsite[,i]), main = paste("Quadrat", i),
-       xlab = "Local species richness", ylab = "", frame = F,
-       xlim = c((min(C[i], out9$sims.list$Nsite[,i], na.rm = T)-2),
-                max(out9$sims.list$Nsite[,i]) ))
-  abline(v = C[i], col = "grey", lwd = 4)
-  browser()
-}
-
-# Plot it only for a selection of sites
-par(mfrow = c(3,3), mar = c(5,4,3,2))
-for(i in c(9, 32, 162, 12, 27, 30, 118, 159, 250)){
-  plot(table(out9$sims.list$Nsite[,i]), main = paste("Quadrat", i),
-       xlab = "Local species richness", ylab = "", frame = F,
-       xlim = c((min(C[i], out9$sims.list$Nsite[,i], na.rm = T)-2),
-                max(out9$sims.list$Nsite[,i]) ))
-  abline(v = C[i], col = "grey", lwd = 4)
-}
-
-
-# Plot posterior distribution of total species richness (Ntotal)
-plot(table(out9$sims.list$Ntotal), main = "", ylab = "", xlab = "Avian metacommunity size in Swiss MHB survey (267 1km2 quadrats)", frame = F, xlim = c(144, 245))
-abline(v = nspec, col = "grey", lwd = 4)
 
 
 
